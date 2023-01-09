fd7e7851bf9766f7b5c3df5f32f5b7cf9e47265b
nefir2 <vadim.ahtamov@gmail.com>
2023-01-09 17:29:29 +0300

    Добавьте файлы проекта.

---
+++ fakeopen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace fakeopen
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				try
+				{
+					if (args.Length != 0)
+					{
+						Process.Start(args[0]);
+						break;
+					}
+					else
+					{
+						Console.Write("Path to program: ");
+						Process.Start(Console.ReadLine());
+						break;
+					}
+				}
+				catch (Exception ex) 
+				{
+					Console.WriteLine(ex.Message);
+					args = null;
+					continue;
+				}
+			}
+		}
+	}
+}

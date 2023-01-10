6dbb77c8d4f073d069ca501f5e045d6c42795137
nefir2 <vadim.ahtamov@gmail.com>
2023-01-10 07:02:14 +0300

    using lib to hide console before program starts

--- fakeopen/Program.cs
+++ fakeopen/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace fakeopen
 {
@@ -7,6 +8,7 @@ namespace fakeopen
 	{
 		static void Main(string[] args)
 		{
+			if (args.Length != 0) { Hider(true); }
 			while (true)
 			{
 				try
@@ -25,11 +27,35 @@ namespace fakeopen
 				}
 				catch (Exception ex) 
 				{
+					Hider(false);
 					Console.WriteLine(ex.Message);
 					args = null;
 					continue;
 				}
 			}
+		}
+
+		private static IntPtr hWnd;
+		//https://habr.com/ru/company/otus/blog/598409/?ysclid=lacmcadxbe475507780
+		[DllImport("user32.dll", SetLastError = true)][return: MarshalAs(UnmanagedType.Bool)] public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, int uFlags);
+		[DllImport("user32.dll")] public static extern int MessageBox(IntPtr hwnd, String text, String caption, int options);
+		private const int HWND_TOPMOST_ON = -1;
+		private const int HWND_TOPMOST_OFF = -2; //добавлено собственноручно, методом тыка.
+		private const int SWP_NOMOVE = 0x0002;
+		private const int SWP_NOSIZE = 0x0001;
+		[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+		const int SW_HIDE = 0;
+		const int SW_SHOW = 5;
+		[DllImport("kernel32.dll")] public static extern IntPtr GetConsoleWindow();
+		[DllImport("kernel32.dll")] public static extern void ExitProcess([In] uint uExitCode);
+		/// <summary>
+		/// скрытие главного окна.
+		/// </summary>
+		/// <param name="isHidden"><see langword="true"/> чтобы скрыть, <br/><see langword="false"/> чтобы показать.</param>
+		public static void Hider(bool isHidden)
+		{
+			hWnd = GetConsoleWindow();
+			ShowWindow(hWnd, isHidden ? SW_HIDE : SW_SHOW);
 		}
 	}
 }

041bd738ee818f8de0f124ae37090c6ad3e2be75
nefir2 <vadim.ahtamov@gmail.com>
2023-03-06 03:18:05 +0300

    opener with args by file dialog and console logs

--- fakeopen/Program.cs
+++ fakeopen/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -12,28 +13,32 @@ namespace fakeopen
 			bool isHidden = false;
 			if (args.Length != 0)
 			{
+				Console.WriteLine("Args exists.");
 				Hider(true);
 				isHidden = true;
 			}
+			else Console.WriteLine("there's no args with start.");
 			while (true)
 			{
 				try
 				{
 					if (!(args is null) && args.Length != 0)
 					{
+						Console.WriteLine($"Starting file \"{args[0]}\" with parameters {ConcatArgs(KillFirst(args))}");
 						Start(GetFileAndArgs(args).ToTuple());
 						break;
 					}
 					else
 					{
+						Console.WriteLine("starting OpenFileDialog...");
 						OpenFileDialog ofd = new OpenFileDialog() { Multiselect = true, CheckFileExists = false, CheckPathExists = false };
 						if (ofd.ShowDialog() == DialogResult.OK)
 						{
-							foreach (var filename in ofd.SafeFileNames)
-							{
-								Console.WriteLine($"choosed: {filename}");
-							}
-							Start(GetFileAndArgs(ofd.SafeFileNames).ToTuple());
+							foreach (var filename in ofd.SafeFileNames) Console.WriteLine($"choosed filenames: {filename}");
+							List<string> s = new List<string>(ofd.SafeFileNames);
+							s.RemoveAt(0);
+							Console.WriteLine($"Starting file \"{ofd.FileNames[0]}\" with parameters {ConcatArgs(s.ToArray())}");
+							Start(ofd.FileNames[0], ConcatArgs(s.ToArray()));
 						}
 						break;
 					}
@@ -41,8 +46,13 @@ namespace fakeopen
 				catch (Exception ex) 
 				{
 					if (isHidden) Hider(false);
-					Console.WriteLine(ex.Message);
+					Console.WriteLine("OH NO! I GOT AN EXCEPTION!");
+					Console.WriteLine("exception message: " + ex.Message);
+					Console.WriteLine("\ndeleting args...");
+					Console.WriteLine($"program args before: ");
+					ShowArray(args, ",\n", false);
 					args = null;
+					Console.WriteLine($"program args after: {args}");
 					continue;
 				}
 			}
@@ -73,6 +83,28 @@ namespace fakeopen
 			for (int i = 1; i < FileNames.Length; i++) fileargs += $" {FileNames[i]}";
 			fileargs.Trim(' ');
 			return (filename, fileargs);
+		}
+		private static string ConcatArgs(string[] args, char separator = ' ')
+		{
+			string ret = "";
+			for (int i = 1; i < args.Length; i++) ret += $"{separator}{args[i]}";
+			ret.Trim(separator);
+			return ret;
+		}
+		private static T[] KillFirst<T>(T[] array)
+		{
+			List<T> list = new List<T>(array);
+			list.RemoveAt(0);
+			return list.ToArray();
+		}
+		private static void ShowArray<T>(T[] array, string sep = ", ", bool hasEndLine = true)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (i + 1 != array.Length) Console.Write($"{array[i]}{sep}");
+				else Console.Write(array[i]);
+			}
+			if (hasEndLine) Console.WriteLine();
 		}
 
 		private static IntPtr hWnd;
